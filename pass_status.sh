2d4066d085967eaeb7d21ac4e1565a1e528c9857
Blake Garner <blake@netjibbing.com>
2024-01-10 20:41:48 -0800

    pass status

--- pass_status.sh
+++ pass_status.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# source url 
+# source url
 SOURCE_URL="https://wsdot.com/travel/real-time/mountainpasses/stevens"
 
 # selector path
 SELECTOR_PATH='#index > div:nth-child(7) > div.full-width.column-container.mountain-pass > div.column-1'
 
-# save selected html to file
-shot-scraper html $SOURCE_URL -o ~/scratch/wsdot_stevens_pass.html --selector "$SELECTOR_PATH"
+HTML_TEMP_FILE="/Users/blake/scratch/wsdot_stevens_pass.html"
+
+## save selected html to file
+shot-scraper html $SOURCE_URL -o "$HTML_TEMP_FILE" --selector "$SELECTOR_PATH"
+
+# HTML_TEMP_FILE=~/scratch/closed_wsdot_stevens_pass.html
+# HTML_TEMP_FILE=~/scratch/avalanche_wsdot_stevens_pass.html
 
 # use pup to extract the conditions
-PASS_STATUS_EAST=$(cat ~/scratch/wsdot_stevens_pass.html | pup 'body > div > div:nth-child(4) > div.conditionValue' text{})
-PASS_STATUS_WEST=$(cat ~/scratch/wsdot_stevens_pass.html | pup 'body > div > div:nth-child(5) > div.conditionValue' text{})
-
+PASS_STATUS_EAST=$(cat $HTML_TEMP_FILE | pup 'body > div > div:nth-child(4) > div.conditionValue' text{})
+PASS_STATUS_WEST=$(cat $HTML_TEMP_FILE | pup 'body > div > div:nth-child(5) > div.conditionValue' text{})
 PASS_STATUS="Open"
 
 # if path status contains "Closed" then echo "Closed"
@@ -31,9 +35,10 @@ else
   echo "West Open"
 fi
 
-# if path status contains "Closed" then echo "Closed"
+# if the pass is close render the conditions
 if [[ $PASS_STATUS == *"Closed"* ]]; then
-  
+  PASS_CONDITIONS=$(cat $HTML_TEMP_FILE | pup 'body > div > div:nth-child(6) > div.conditionValue' text{} | sed 's/ \{2,\}/ /g' | tr -d '\n')
+  convert -size 250x200 -background white -fill black -pointsize 14 -gravity center -border 5 -bordercolor white caption:"$PASS_CONDITIONS" pass_conditions.png
 else
-  echo "Open"
+  convert -size 250x200 xc:none pass_conditions.png
 fi
